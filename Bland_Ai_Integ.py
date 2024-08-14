0264b3b3f46f82352ea040cb861482f702ae40ab
Thirupathi-Kadari <126477016+Thirupathi-Kadari@users.noreply.github.com>
2024-08-14 17:37:22 -0400

    Update Bland_Ai_Integ.py

--- Bland_Ai_Integ.py
+++ Bland_Ai_Integ.py
@@ -4,12 +4,27 @@ import requests
 from dotenv import load_dotenv
 import os
 
+# Load environment variables from the .env file
 load_dotenv()
 
 def trigger_call():
+    """
+    Triggers a call using the Bland AI API.
+
+    Constructs a payload with call parameters and sends a POST request
+    to the Bland AI API endpoint to initiate the call.
+
+    Returns:
+        str: The response text from the API call.
+    """
+    
+    # API endpoint for initiating a call
     url = "https://api.bland.ai/v1/calls"
+
+    # Retrieve API key from environment variables
     api_key = os.environ.get("API_KEY")
 
+    # Payload for the API request
     payload = {
         "phone_number": "+19797393588",
         "task": "Remind the Customer about their appointment",
@@ -43,12 +58,14 @@ def trigger_call():
         "answered_by_enabled": True
     }
 
+    # Headers for the API request
     headers = {
         "authorization": api_key,
         "Content-Type": "application/json"
     }
 
+    # Send the POST request to the API endpoint
     response = requests.post(url, json=payload, headers=headers)
     
-    # Returning the response for further use
+    # Return the response text for further processing
     return response.text
